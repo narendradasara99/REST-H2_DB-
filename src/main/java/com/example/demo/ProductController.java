fb85efd4f518fc49d713119d54160b9d3358add7
narendra <narendradasara99@gmail.com>
2022-02-15 16:14:42 +0530

    feature 2 change

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
+String s="narendra";
 	@Autowired
 	private ProductRepository repo;
 	@PostMapping(value="/product",consumes = MediaType.APPLICATION_JSON_VALUE)
